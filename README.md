3969ab2e806b221baaed519446502149534ed475
bRuttaZz <agrajpdas@gmail.com>
2025-11-02 17:17:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reverse Tunnel Daemon
+
+**Nothing special a dead-simple automation script for reverse ssh tunnels**
+
+**But why**
+- Nthng wanted a simple script to expose my tcp applications running in home network to the remote ones
+- Yes you're completely free to use autossh :)
+
+**Installation?**
+- The `.deb` build can be found on the release page
---
+++ reverse-tunneld/etc/reverse-tunneld/tunneld.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/sh
+
+if test -z $CONF_FILE
+then
+    CONF_FILE=/etc/reverse-tunneld/tunnel-conf-shell
+    echo "Conf file not specified. Using custom conf file: $CONF_FILE" >&2
+fi
+
+if ! test -f $CONF_FILE
+then
+    echo "Conf file not found! Exiting (101)"
+    exit 101
+fi
+
+if grep -q "HostName <remote address>" $CONF_FILE
+then
+    echo "Conf file not customized: $CONF_FILE"
+    exit 102
+fi
+
+if ! grep -qE '^[[:space:]]*RemoteForward' $CONF_FILE
+then
+    echo "No port configuration found! Exiting (102)"
+    exit 103
+fi
+
+cmd="/usr/bin/ssh -o ExitOnForwardFailure=yes -N -v -F $CONF_FILE remote"
+echo "cmd: '$cmd'\n"
+
+$cmd
